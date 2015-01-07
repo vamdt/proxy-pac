5c9eac362a3bd9db8a5e539830ff6cd572d2d77e
vamdt <liang6688267@gmail.com>
2015-01-07 14:02:08 +0800

    Add proxy.pac gen

---
+++ pac_gen.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import urllib, re
+
+GFWLIST_PAC_URL="https://raw.githubusercontent.com/clowwindy/gfwlist2pac/master/test/proxy.pac"
+CUSTOM_START_COMMENT = "// custom -- start\n"
+CUSTOM_END_COMMENT = "\n// custom -- end"
+
+def main():
+    pac_content = urllib.urlopen(GFWLIST_PAC_URL).read()
+    repl = '\\n' + custom_domains() + '\\1'
+
+    ret = re.sub(r'(\s.+: 1\n\};\n)', repl, pac_content, re.DOTALL)
+    save(ret, 'proxy.pac')
+
+def json_filed(field):
+    return "  \"" +  field + "\": 1,"
+
+def custom_domains():
+    fp = open('./proxy_domains.txt', 'r')
+    content = fp.read()
+    domains = content.strip().split("\n")
+    domains_format = '\n'.join(str(x) for x in map(json_filed, domains))
+    return CUSTOM_START_COMMENT + domains_format + CUSTOM_END_COMMENT
+
+def save(obj, name):
+    file = open(name, 'w')
+    file.write(str(obj))
+    file.close
+
+
+
+if __name__ == '__main__':
+    main()
